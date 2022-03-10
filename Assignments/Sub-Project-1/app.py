7231082fcb4f527bef7eefd902be18a775f6ef8b
Asif Mahmud <asifmahmud3472@gmail.com>
2022-03-09 21:33:48 -0500

    sub-project-1 done

--- Assignments/Sub-Project-1/app.py
+++ Assignments/Sub-Project-1/app.py
@@ -43,30 +43,39 @@ class MainWindow(QWidget):
         # setting the title of the window
         self.setWindowTitle("Weather App")
 
-        title = QLabel('Check the weather of a place here')
-        title.setStyleSheet("color:white; font-weight: bold; font-size: 20px; text-align: center")
-        title.setContentsMargins(20, 10, 20, 0)
-        title.setAlignment(QtCore.Qt.AlignCenter)
+        self.no_data = QLabel("")
+        self.no_data.setAlignment(QtCore.Qt.AlignCenter)
 
+        #setting the headline of the app
+        headline = QLabel('Check the weather of a place here')
+        headline.setStyleSheet("color:white; font-weight: bold; font-size: 20px; text-align: center") #style for the headline
+        headline.setContentsMargins(20, 10, 20, 0)
+        headline.setAlignment(QtCore.Qt.AlignCenter)
+
+        #input box for the city
         self.placeName = QLineEdit()
-        self.placeName.setPlaceholderText("Enter the name of the place")
+        self.placeName.setPlaceholderText("Enter the name of the place") #place holder for the input box
         self.placeName.setContentsMargins(20,5,20,5)
-        self.placeName.setStyleSheet('font-size:15px; height: 40px;')
+        self.placeName.setStyleSheet('font-size:15px; height: 40px;') #style for the input
 
+        #creating the search button
         searchBtn = QPushButton('Search')
         searchBtn.setStyleSheet("background: #337ab7; color: white; font-weight: bold; border-radius: 6px; width: 100px; height: 35px;")
-        searchBtn.clicked.connect(self.get_data)
+        searchBtn.clicked.connect(self.getData)
 
+        #creating the layouts for the app
         inputLayout = QHBoxLayout()
         self.outerLayout = QVBoxLayout()
         topLayout = QFormLayout()
         bottomLayout = QVBoxLayout()
 
+        #adding the widgets into the layouts
         inputLayout.addWidget(self.placeName)
         inputLayout.addWidget(searchBtn)
-        topLayout.addRow(title)
+        topLayout.addRow(headline)
         topLayout.addRow(inputLayout)
 
+        #styles and labels for the result
         styles = "color: white; font-weight:bold; font-size:18px;"
         self.weather = QLabel()
         self.temp_fahrenheit = QLabel()
@@ -75,22 +84,26 @@ class MainWindow(QWidget):
         self.humidity = QLabel() 
         self.image = QLabel()
 
-        #for timer
+        #timer for the next update
         self.count = 0
-        self.flag = False
+        self.start = False
         self.timer_label = QLabel(self)
-        self.timer_label.setText("")
         self.timer_label.setAlignment(QtCore.Qt.AlignCenter)
+        timer = QtCore.QTimer(self) #creating a timer object
+        timer.timeout.connect(self.showTime) #adding action to timer
+        timer.start(100)
         
+        #setting the styles for the result widgets
         self.weather.setAlignment(QtCore.Qt.AlignCenter)
-
         self.weather.setStyleSheet(styles)
         self.temp_fahrenheit.setStyleSheet(styles)
         self.temp_celsius.setStyleSheet(styles)
         self.wind_speed.setStyleSheet(styles)
         self.humidity.setStyleSheet(styles)
         self.timer_label.setStyleSheet(styles)
+        self.no_data.setStyleSheet(styles)
 
+        #layout for the results
         bottomLayoutTop = QHBoxLayout()
         bottomLayoutTop.addWidget(self.weather)
         bottomLayoutTop.addWidget(self.image)
@@ -103,44 +116,54 @@ class MainWindow(QWidget):
         bottomLayoutBottom.addWidget(self.wind_speed)
         bottomLayoutBottom.addWidget(self.humidity)
         
-
+        #adding all the layouts together
         bottomLayout.addLayout(bottomLayoutTop)
         bottomLayout.addLayout(bottomLayoutMiddle)
         bottomLayout.addLayout(bottomLayoutBottom)
 
         topLayout.addRow(bottomLayout)
         self.outerLayout.addLayout(topLayout)
+        self.outerLayout.addWidget(self.no_data)
         self.outerLayout.addWidget(self.timer_label)
-        # self.outerLayout.addLayout(bottomLayout)
 
+        #adding all the layouts to the out most layout
         self.setLayout(self.outerLayout)
         
         # setting  the size of window
         self.setFixedSize(700, 500)
 
     def showTime(self):
+        if self.start:
+            #decreasing the counter value
+            self.count -= 1
+            #checking if the timer is done and calling the function to call the api again
+            if self.count == 0: 
+                self.start = False
+                self.getData()
   
-        # checking if flag is true
-        if self.flag:
-            self.count+= 1
-
-        text = str(self.count / 10)
-        self.timer_label.setText(text)
+        if self.start:
+            #setting text for the counter label
+            text = str(self.count / 10) + " s"
+            self.timer_label.setText(text)
 
     def resetTimer(self):
-        self.flag = False
-        self.count = 0
+        self.start = False
+        #setting the counter to 1800s (1800s = 30m)
+        self.count = 18000
         self.timer_label.setText("")
     
     def startTimer(self):
-        self.flag = True
+        self.start = True
+        if self.count == 0:
+            self.start = False
 
-    def get_data(self):
+    def getData(self):
+        #getting the value from the text box
         city = self.placeName.text()
         self.doRequest(city)
 
-    def doRequest(self, city):   
-        
+    def doRequest(self, city):  
+        #creating the usl for api call 
         url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=703d74f2eac3edcf47136e9d91a0768d&units=imperial"
         req = QtNetwork.QNetworkRequest(QtCore.QUrl(url))
         
@@ -148,8 +171,10 @@ class MainWindow(QWidget):
         self.nam.finished.connect(self.handleResponse)
         self.nam.get(req)  
         
+    #method for clearing all the data
     def clearData(self):
-        self.weather.setText("No data found")
+        self.no_data.setText("No data found")
+        self.weather.setText("")
         self.temp_celsius.setText("")
         self.temp_fahrenheit.setText("")
         self.humidity.setText("")
@@ -158,20 +183,17 @@ class MainWindow(QWidget):
         self.timer_label.setText("")
 
     def handleResponse(self, reply):
-
         er = reply.error()
-        
+        #if there's no error
         if er == QtNetwork.QNetworkReply.NoError:
+            self.no_data.setText("")
             bytes_string = reply.readAll()
             data_dictionary = json.loads(str(bytes_string, "utf-8"))
             self.process_data(data_dictionary)
 
-            # creating a timer object
-            timer = QtCore.QTimer(self)
-            # adding action to timer
-            timer.timeout.connect(self.showTime)
-            # update the timer every tenth second
-            timer.start(100)
+            self.count = 18000
+            # setting text to the label
+            self.timer_label.setText(str(self.count))
             self.startTimer()
         else:
             self.clearData()
@@ -180,20 +202,23 @@ class MainWindow(QWidget):
             print(reply.errorString())
 
     def process_data(self, data):
+        #assigning all the data to their corresponding variable
         temp_fahrenheit = data["main"]["temp"]
         temp_degree = round(((temp_fahrenheit - 32) * 0.5556), 2)
         wind_speed = data["wind"]["speed"]
         humidity = data["main"]["humidity"]
         weather = data["weather"][0]["main"]
         
+        #getting the icon for the weather
         icon_url = 'http://openweathermap.org/img/wn/'+data["weather"][0]["icon"]+'@2x.png'
         icon = urllib.request.urlopen(icon_url).read()
         image = QImage()
         image.loadFromData(icon)
         
+        #setting the text for all the labels for the results
         self.weather.setText("Weather: " + weather)
-        self.temp_celsius.setText("Temperature (Fahrenheit): " + str(temp_fahrenheit))
-        self.temp_fahrenheit.setText("Temperature (Celsius): " + str(temp_degree))
+        self.temp_celsius.setText("Temperature (Fahrenheit): " + str(temp_fahrenheit) + "F")
+        self.temp_fahrenheit.setText("Temperature (Celsius): " + str(temp_degree) + "\N{DEGREE SIGN}C")
         self.humidity.setText("Wind Speed (miles/hour): " + str(wind_speed))
         self.wind_speed.setText("Humidity: " + str(humidity))
         pixmap = QPixmap(image)
